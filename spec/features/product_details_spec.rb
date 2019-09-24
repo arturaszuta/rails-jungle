052e52fba34702d34a58ad2466d8d6623a38232c
Arturas Zuta <arturaszuta@gmail.com>
2019-09-24 13:20:53 +0000

    Add testing for show product page

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
+
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "They see all products" do
+    # ACT
+    visit root_path
+
+    # DEBUG / VERIFY
+    # expect(page).to have_css 'article.product', count: 10
+    # click_link 'Super shirt'
+    # find('article').click
+    find('article h4', match: :first).click
+
+    sleep 4
+
+    expect(page).to have_content 'Quantity' 
+
+  end
+
+end
